8a094b2f904f4bfb8c5bc0f94d581aa09de9c13d
Shianne <slesure1@ggc.edu>
2024-05-02 18:08:40 -0400

    add database to test

--- Test/NewTest.java
+++ Test/NewTest.java
@@ -6,18 +6,17 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RunWith(JUnitParamsRunner.class)
@@ -27,16 +26,15 @@ public class NewTest {
     private static Connection connection;
     private static final String DATE_PATTERN = "yyyy-MM-dd";
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
-    private static String DB_URL = "jdbc:sqlite:hotelcheckin.sqlite";
+    private static String DB_URL = "jdbc:sqlite:tester.sqlite";
     private static List<LocalDate> hotelDates = new ArrayList<>();
-    private final String[] hotelChains = {"Holiday Inn", "Hyatt Regency", "Hilton", "Comfort Suites", "Hampton Inn"};
+    //private final String[] hotelChains = {"Holiday Inn", "Hyatt Regency", "Hilton", "Comfort Suites", "Hampton Inn"};
 
     private final String[] cities = {"Sacramento", "Atlanta", "Orlando", "Miami", "Austin"};
 
     @BeforeClass
     public static void setUp() throws SQLException {
         connection = DriverManager.getConnection(DB_URL);
-        //driver = new ChromeDriver();
         driver = new FirefoxDriver();
         LocalDate startDate = LocalDate.of(2024, 5, 5);
         LocalDate endDate = LocalDate.of(2024, 5, 31);
@@ -46,8 +44,11 @@ public class NewTest {
         }
     }
 
+    /*
+
     @Test
     @Parameters({"Atlanta", "Orlando", "Sacramento", "Miami", "Austin"})
+
     public void testGetPrice(String location) {
         for(int k = 0; k < hotelChains.length; k ++) {
             for (int i = 0; i < hotelDates.size(); i++) {
@@ -63,54 +64,43 @@ public class NewTest {
 
     }
 
+     */
+
     @Test
     @Parameters({"Hilton", "Hyatt Regency", "Hampton Inn", "Holiday Inn", "Comfort Suites" })
     public void testGetHotelPrice(String hotels){
         for(int k = 0; k < cities.length; k ++) {
             for (int i = 0; i < hotelDates.size(); i++) {
                 String url = buildUrl(cities[k].toString(), hotels , hotelDates.get(i).toString(), 25, true);
-                //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                 driver.get(url);
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-                //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
                 if(!driver.findElement(By.id("results_list_lowest_price")).isEnabled() || driver.getTitle().contains("gateway")){
                     hotelDates.get(i).plusDays(1);
                 } else {
-                   // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-                    //WebElement price = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("amount")));
                     WebElement price = driver.findElement(By.className("amount"));
                     System.out.println(cities[k].toString() + " - " + hotels + " - " + hotelDates.get(i).toString() + " - $" + price.getText());
                     System.out.println();
+
+                    String timeStamp = (new Date()).toString();
+                    insertHotelPricesToDatabase(hotels, cities[k].toString(), hotelDates.get(i).toString(), price.getText(), timeStamp);
                 }
             }
         }
     }
 
-    private void getHotel(String cities, String hotels){
-        for (int i = 0; i < hotelDates.size(); i++) {
-            String url = buildUrl(cities, hotels , hotelDates.get(i).toString(), 25, true);
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(url);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-            //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            if(!driver.findElement(By.id("results_list_lowest_price")).isEnabled() || driver.getTitle().contains("gateway")){
-                hotelDates.get(i).plusDays(1);
-            } else {
-                // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-                //WebElement price = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("amount")));
-                WebElement price = driver.findElement(By.className("amount"));
-                System.out.println(cities + " - " + hotels + " - " + hotelDates.get(i).toString() + " - $" + price.getText());
-                System.out.println();
-            }
-                /*
-                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-                WebElement price = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("amount")));
+    private void insertHotelPricesToDatabase (String hotels, String city, String date, String price, String time){
+        String sql = "insert into test values (null,?,?,?,?,?)";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1,hotels);
+            ps.setString(2,city);
+            ps.setString(3,date);
+            ps.setString(4,price);
+            ps.setString(5,time);
+            ps.executeUpdate();
 
-                //WebElement price = driver.findElement(By.cssSelector("#amount"));
-                System.out.println(cities[k].toString() + " - " + hotels + " - " + hotelDates.get(i).toString() + " - $" + price.getText());
-                System.out.println();
-
-                 */
+        } catch (SQLException e){
+            e.printStackTrace();
         }
     }
 
